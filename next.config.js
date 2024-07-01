4665428aa24267b0d2cf07fefd5dc43808d5fe32
Vinogren David <Vinogren.David@derivco.co.im>
2024-07-01 02:40:08 +0100

    Updates

--- next.config.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-    // other configurations...
-    assetPrefix: process.env.NODE_ENV === 'production' ? '/chocolicious' : '',
-    output: {
-        fileSystem: process.env.NODE_ENV !== 'production',
-    },
-}
